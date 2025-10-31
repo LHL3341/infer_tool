4d355f1eaa4f03be8604a7e9db0cbf29b5a073cf
linhonglin <linhonglin03@gmail.com>
2025-10-31 18:12:15 +0800

    fix hf infer bug

--- run_qwen3vl_score.sh
+++ run_qwen3vl_score.sh
@@ -1,14 +1,14 @@
 cd /mnt/dhwfile/raise/user/linhonglin/data_process/infer_tool
 
 bash batch_infer.sh \
-  --input /mnt/dhwfile/raise/user/linhonglin/vlm/rlpr/results_merged_success_all.jsonl \
+  --input /mnt/dhwfile/raise/user/linhonglin/vlm/rlpr/results_merged_success_others_sample500.jsonl \
   --model_path /mnt/dhwfile/raise/user/linhonglin/vlm/models/qwen3vl_tikzgen_score \
   --prompt_name score \
   --model_name qwen3vl_nothink \
   --parts 1 \
   --gpus 1 \
   --temperature 0.1 \
-  --n_sample 1 \
+  --n_sample 3 \
   --backend hf \
   --chunk_size 8 \
   --partition belt_road
